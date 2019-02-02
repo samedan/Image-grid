cddcccbe17ac9c7eeaf0e5168f8b249d8c07eed0
samedan <same.dan@gmail.com>
2019-02-02 21:42:11 +0100

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-      'Client-ID 144f01324d5c579f66b8ee4ec731fca681781592daf223f18c0f5552ba84ede4'
+      'Client-ID ****'
   }
 });
